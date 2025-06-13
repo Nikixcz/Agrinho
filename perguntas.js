312920ca7f140a138fded0637f6acfbd71adbf3e
Nikixcz <conti.nicolas@escola.pr.gov.br>
2025-06-13 11:03:41 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+
+criaCartao(
+    'Natureza',
+    'Quais os impactos do desmatamento?',
+    'O desmatamento prejudica a qualidade do ar, extingue vidas selvagens, um exemplo aqui no Brasil é São paulo que não tem muitas árvores e o ar de lá é extremamente poluido'
+)
+criaCartao(
+    'Natureza',
+    'Quais os impactos das queimadas?',
+    'As queimadas prejudicam o ecossistema de diversas formas, como por exemplo o desmatamento de árvores, muitos animais são vitimas de queimadas, e o C02 que é liberado por elas é prejudicial a nossa saúde.'
+)
+criaCartao(
+    'Natureza',
+    'O que podemos fazer para ajudar a ter um mundo melhor?',
+    'Não jogar lixos na rua, não jogar bitucas de cigarro ou semelhantes perto de florestas, plantar uma árvore na frente de sua casa se possível e não descartar o lixo incorretamente'
+)
+criaCartao(
+    'Campo e Cidade',
+    'Qual o melhor campo ou cidade?',
+    'Ambos tem sua importancia, e devem coexistir, o campo é melhor para a natureza pois não tem tanta produção prejudicial como C02 ou outros poluentes, porém a cidade é muito mais confortável para nós humanos, mas é pior para a natureza'
+)
+criaCartao(
+    'Campo e Cidade',
+    'Qual o melhor campo ou cidade?',
+    ''
+)
+criaCartao(
+    'Campo e Cidade',
+    'Qual é a relação entre o campo e a cidade?',
+    'é de interdependência, ou seja, ambos dependem um do outro para prosperarem'
+)
+criaCartao(
+    'Campo e Cidade',
+    'O que a cidade e o campo têm em comum?',
+    'A produção de alimentos no campo é fundamental para abastecer as cidades, e a cidade, por sua vez, oferece produtos e serviços para o campo. Além disso, ambos são espaços habitados, onde a população se organiza e interage'
+)
+criaCartao(
+    'Campo e Cidade',
+    'Qual a importância do campo e da cidade?',
+    'O campo, como espaço rural, fornece matérias-primas e recursos naturais como alimentos, água e energia, enquanto a cidade, como espaço urbano, oferece produtos manufaturados, serviços especializados e oportunidades de trabalho, formando uma relação de interdependência fundamental'
+)
